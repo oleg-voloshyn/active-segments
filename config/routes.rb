7ea4c10c44eed712cf6eeae7c836401b52576da7
oleg-voloshyn <voloshyn@active-bridge.com>
2020-07-04 22:24:50 +0300

    Add About page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :results, only: %i[index show]
   resources :users, only: %i[index show]
   resources :segments, only: %i[index show]
-  get ':page', to: 'pages#show', as: :page, constraints: { page: /(prizes|position|points|faq)/ }
+  get ':page', to: 'pages#show', as: :page, constraints: { page: /(prizes|position|points|faq|about)/ }
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
