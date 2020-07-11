a31dbbf7317dde6c8a16a4085c153d23b94fd67f
oleg-voloshyn <voloshyn@active-bridge.com>
2020-07-11 18:37:54 +0300

    Reverse

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -4,9 +4,9 @@ class ResultsController < InheritedResources::Base
     race = Race.last
     @segments = race.segments.order(:id)
     @users = if params[:gender] == "female"
-               race.users.where(gender: "female").sort_by{ |user| user.results.pluck(:points).sum }
+               race.users.where(gender: "female").sort_by{ |user| user.results.pluck(:points).sum }.reverse
              else
-               race.users.where(gender: "male").sort_by{ |user| user.results.pluck(:points).sum }
+               race.users.where(gender: "male").sort_by{ |user| user.results.pluck(:points).sum }.reverse
              end
   end
 
