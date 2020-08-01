b8ecc2f3be3d794ad43ce1a587169c0db87fa4fe
oleg-voloshyn <voloshyn@active-bridge.com>
2020-08-01 12:05:04 +0300

    remove condition

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,20 +16,18 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 $(document).ready(function(){
-  if ($("#table-1").length) {
-    var tableOffset = $("#table-1").offset().top;
-    var $header = $("#table-1 > thead").clone();
-    var $fixedHeader = $("#header-fixed").append($header);
+  var tableOffset = $("#table-1").offset().top;
+  var $header = $("#table-1 > thead").clone();
+  var $fixedHeader = $("#header-fixed").append($header);
 
-    $(window).bind("scroll", function() {
-        var offset = $(this).scrollTop();
+  $(window).bind("scroll", function() {
+      var offset = $(this).scrollTop();
 
-        if (offset >= tableOffset && $fixedHeader.is(":hidden")) {
-            $fixedHeader.show();
-        }
-        else if (offset < tableOffset) {
-            $fixedHeader.hide();
-        }
-    });
-  }
+      if (offset >= tableOffset && $fixedHeader.is(":hidden")) {
+          $fixedHeader.show();
+      }
+      else if (offset < tableOffset) {
+          $fixedHeader.hide();
+      }
+  });
 });
