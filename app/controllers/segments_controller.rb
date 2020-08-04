baa9ebb84796bc43b094c813397f7cc798a25d64
oleg-voloshyn <voloshyn@active-bridge.com>
2020-08-04 18:11:03 +0300

    Add show segment page

--- app/controllers/segments_controller.rb
+++ app/controllers/segments_controller.rb
@@ -5,6 +5,18 @@ class SegmentsController < InheritedResources::Base
     @segments = race.segments
   end
 
+  def show
+    race = Race.last
+
+    @users = if params[:gender] == "female"
+               race.users.where(gender: "female").sort_by{ |user| user.results.pluck(:points).sum }.reverse
+             else
+               race.users.where(gender: "male").sort_by{ |user| user.results.pluck(:points).sum }.reverse
+             end
+    @segment = Segment.find(params[:id])
+    @lider = @users.first.results.where(segment_id: @segment.id).pluck(:points).sum
+  end
+
   private
 
   def segment_params
