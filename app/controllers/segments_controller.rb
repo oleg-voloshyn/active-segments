3bc3678668f3d0c5295097794bd32786bd6e352c
oleg-voloshyn <voloshyn@active-bridge.com>
2020-08-04 18:19:04 +0300

    Fix segment page

--- app/controllers/segments_controller.rb
+++ app/controllers/segments_controller.rb
@@ -8,12 +8,12 @@ class SegmentsController < InheritedResources::Base
   def show
     race = Race.last
 
-    @users = if params[:gender] == "female"
-               race.users.where(gender: "female").sort_by{ |user| user.results.pluck(:points).sum }.reverse
-             else
-               race.users.where(gender: "male").sort_by{ |user| user.results.pluck(:points).sum }.reverse
-             end
     @segment = Segment.find(params[:id])
+    @users = if params[:gender] == "female"
+               race.users.where(gender: "female").sort_by{ |user| user.results.where(segment_id: @segment.id).pluck(:points).sum }.reverse
+             else
+               race.users.where(gender: "male").sort_by{ |user| user.results.where(segment_id: @segment.id).pluck(:points).sum }.reverse
+             end
     @lider = @users.first.results.where(segment_id: @segment.id).pluck(:points).sum
   end
 
